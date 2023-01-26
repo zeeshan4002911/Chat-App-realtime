cced7195293046b15812ed9a7aa6ae19029c0750
AAA4002911 <zeeshan4002911@gmail.com>
2023-01-26 19:47:26 +0530

    Design Changed

--- src/components/Toolbox.jsx
+++ src/components/Toolbox.jsx
@@ -1,45 +1,71 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { logOut } from "../firebase/auth";
 import { auth } from "../firebase/config";
-import { Button, Avatar } from "@mui/material";
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import { useState, useEffect } from "react";
+import { Avatar, Menu, MenuItem, IconButton } from "@mui/material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 const Toolbox = () => {
     const navigate = useNavigate();
     const [currentUser, setCurrentUser] = useState(auth);
-    
+
     useEffect(() => {
         setCurrentUser(auth);
     }, [auth])
-    
+
     const handleLogout = async () => {
         await logOut(currentUser);
         navigate("/", { replace: true });
     }
 
+    const ITEM_HEIGHT = 50;
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     return (
-        <>
-            <PopupState variant="popover" popupId="popup-menu" >
-                {(popupState) => (
-                    <>
-                        <Button variant="contained" {...bindTrigger(popupState)} /* style={{ width: "fit-content" }} */>
-                            Dashboard
-                        </Button>
-                        <Menu {...bindMenu(popupState)}>
-                            <MenuItem onClick={popupState.close}>
-                                <Avatar alt="Remy Sharp" src={currentUser?.currentUser?.photoURL} referrerPolicy="no-referrer"/>
-                            </MenuItem>
-                            <MenuItem onClick={popupState.close}>{currentUser?.currentUser?.displayName}</MenuItem>
-                            <MenuItem onClick={popupState.close}>My account</MenuItem>
-                            <MenuItem onClick={() => { popupState.close(); handleLogout() }}>Logout</MenuItem>
-                        </Menu>
-                    </>
-                )}
-            </PopupState>
-        </>
+        <div>
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        maxHeight: ITEM_HEIGHT * 5,
+                        width: 'auto',
+                    },
+                }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <Avatar alt="Remy Sharp" src={currentUser?.currentUser?.photoURL} referrerPolicy="no-referrer" />
+                </MenuItem>
+                <MenuItem onClick={handleClose}>{currentUser?.currentUser?.displayName}</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
+            </Menu>
+        </div>
     )
 }
 
